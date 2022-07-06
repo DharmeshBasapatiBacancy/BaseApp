71c46e2ca7a9640dcfe9e927966c7fbc298d9b5e
DharmeshBasapatiBacancy <dharmesh.basapati@bacancy.com>
2022-07-06 18:02:56 +0530

    Added Home Screen with BottomNavigation and some demo fragemnts.

---
+++ app/src/main/java/com/example/baseapp/views/fragments/HomeFragment.kt
@@ -0,0 +1,78 @@
+package com.example.baseapp.views.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.example.baseapp.databinding.FragmentHomeBinding
+import com.example.baseapp.utils.ApiResponse
+import com.example.baseapp.utils.NetworkUtils
+import com.example.baseapp.viewmodels.UserViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    init {
+
+    }
+
+    private val userViewModel: UserViewModel by viewModels()
+
+    private lateinit var contentView: View
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        if (!::contentView.isInitialized) {
+            binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+            contentView = binding.root
+            getThatJoke()
+            binding.apply {
+                getThatJokeButton.setOnClickListener {
+                    if (NetworkUtils.isNetworkConnected(requireContext())) {
+                        getThatJoke()
+                    } else {
+                        binding.thatJokeTextView.text = "No internet connection !!!"
+                    }
+                }
+            }
+        }
+
+        return contentView
+    }
+
+    private fun getThatJoke() {
+        lifecycleScope.launch {
+            userViewModel.getThatJoke().observe(requireActivity()) {
+                when (it) {
+                    is ApiResponse.Success -> {
+                        if (it.data?.attachments?.isNotEmpty()!!) {
+                            binding.thatJokeTextView.text = it.data.attachments[0].text
+                        }
+                    }
+                    is ApiResponse.Error -> {
+                        binding.thatJokeTextView.text = "Try again !!!"
+                    }
+                    is ApiResponse.Loading -> {
+                        binding.thatJokeTextView.text = "Loading..."
+                    }
+                    else -> {
+                        binding.thatJokeTextView.text = "Try again !!!"
+                    }
+                }
+            }
+        }
+    }
+
+    companion object {
+    }
+}
